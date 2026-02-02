7de26eafb16314eda8a9f7c9386bb0186fd8dfa6
Barrett Ruth <br.barrettruth@gmail.com>
2026-02-02 16:37:51 -0500

    feat: diffsplit support

--- plugin/fugitive-ts.lua
+++ plugin/fugitive-ts.lua
@@ -9,3 +9,14 @@ vim.api.nvim_create_autocmd('FileType', {
     require('fugitive-ts').attach(args.buf)
   end,
 })
+
+vim.api.nvim_create_autocmd('OptionSet', {
+  pattern = 'diff',
+  callback = function()
+    if vim.wo.diff then
+      require('fugitive-ts').attach_diff()
+    else
+      require('fugitive-ts').detach_diff()
+    end
+  end,
+})
