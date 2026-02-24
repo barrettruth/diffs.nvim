8dfe3fa994e9644ce8be1475613fcd2ba71a3bd6
Barrett Ruth <br.barrettruth@gmail.com>
2026-02-24 12:03:37 -0500

    fix(conflict)!: change default nav keymaps from ]x/[x to ]c/[c
    Problem: the default conflict navigation keymaps ]x and [x are
    non-standard. Vim uses ]c/[c natively for diff navigation, making
    them far more intuitive for conflict navigation as well.
    
    Solution: change the default keymaps for conflict.keymaps.next and
    conflict.keymaps.prev to ]c and [c respectively. Users who explicitly
    configured ]x/[x will need to update their config.

--- lua/diffs/init.lua
+++ lua/diffs/init.lua
@@ -152,8 +152,8 @@ local default_config = {
       theirs = 'dot',
       both = 'dob',
       none = 'don',
-      next = ']x',
-      prev = '[x',
+      next = ']c',
+      prev = '[c',
     },
   },
 }
