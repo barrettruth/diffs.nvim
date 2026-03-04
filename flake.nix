afd27a269b8570c12a0de054bed672b71d2ca5fa
Barrett Ruth <br.barrettruth@gmail.com>
2026-03-04 14:15:37 -0500

    ci: nix

--- flake.nix
+++ flake.nix
@@ -16,15 +16,26 @@
       forEachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
     in
     {
+      formatter = forEachSystem (pkgs: pkgs.nixfmt-tree);
+
       devShells = forEachSystem (pkgs: {
-        default = pkgs.mkShell {
+        default =
+          let
+            ts-plugin = pkgs.vimPlugins.nvim-treesitter.withPlugins (p: [ p.diff ]);
+            diff-grammar = pkgs.vimPlugins.nvim-treesitter-parsers.diff;
+            luaEnv = pkgs.luajit.withPackages (ps: with ps; [ busted nlua ]);
+            busted-with-grammar = pkgs.writeShellScriptBin "busted" ''
+              nvim_bin=$(which nvim)
+              tmpdir=$(mktemp -d)
+              trap 'rm -rf "$tmpdir"' EXIT
+              printf '#!/bin/sh\nexec "%s" --cmd "set rtp+=${ts-plugin}/runtime" --cmd "set rtp+=${diff-grammar}" "$@"\n' "$nvim_bin" > "$tmpdir/nvim"
+              chmod +x "$tmpdir/nvim"
+              PATH="$tmpdir:$PATH" exec ${luaEnv}/bin/busted "$@"
+            '';
+          in
+          pkgs.mkShell {
           packages = [
-            (pkgs.luajit.withPackages (
-              ps: with ps; [
-                busted
-                nlua
-              ]
-            ))
+            busted-with-grammar
             pkgs.prettier
             pkgs.stylua
             pkgs.selene
