053ff181f0fb23a5f2ff710a2e0373e0f573d2ec
Barrett Ruth <62671086+barrettruth@users.noreply.github.com>
2026-03-21 15:50:43 -0400

    build: separate ci devshell to avoid overriding system neovim (#201)

--- flake.nix
+++ flake.nix
@@ -21,37 +21,42 @@
     {
       formatter = forEachSystem (pkgs: pkgs.nixfmt-tree);
 
-      devShells = forEachSystem (pkgs: {
-        default =
-          let
-            ts-plugin = pkgs.vimPlugins.nvim-treesitter.withPlugins (p: [ p.diff ]);
-            diff-grammar = pkgs.vimPlugins.nvim-treesitter-parsers.diff;
-            luaEnv = pkgs.luajit.withPackages (
-              ps: with ps; [
-                busted
-                nlua
-              ]
-            );
-            busted-with-grammar = pkgs.writeShellScriptBin "busted" ''
-              nvim_bin=$(which nvim)
-              tmpdir=$(mktemp -d)
-              trap 'rm -rf "$tmpdir"' EXIT
-              printf '#!/bin/sh\nexec "%s" --cmd "set rtp+=${ts-plugin}/runtime" --cmd "set rtp+=${diff-grammar}" "$@"\n' "$nvim_bin" > "$tmpdir/nvim"
-              chmod +x "$tmpdir/nvim"
-              PATH="$tmpdir:$PATH" exec ${luaEnv}/bin/busted "$@"
-            '';
-          in
-          pkgs.mkShell {
-            packages = [
-              busted-with-grammar
-              pkgs.prettier
-              pkgs.stylua
-              pkgs.neovim
-              pkgs.selene
-              pkgs.lua-language-server
-              vimdoc-language-server.packages.${pkgs.system}.default
-            ];
+      devShells = forEachSystem (
+        pkgs:
+        let
+          ts-plugin = pkgs.vimPlugins.nvim-treesitter.withPlugins (p: [ p.diff ]);
+          diff-grammar = pkgs.vimPlugins.nvim-treesitter-parsers.diff;
+          luaEnv = pkgs.luajit.withPackages (
+            ps: with ps; [
+              busted
+              nlua
+            ]
+          );
+          busted-with-grammar = pkgs.writeShellScriptBin "busted" ''
+            nvim_bin=$(which nvim)
+            tmpdir=$(mktemp -d)
+            trap 'rm -rf "$tmpdir"' EXIT
+            printf '#!/bin/sh\nexec "%s" --cmd "set rtp+=${ts-plugin}/runtime" --cmd "set rtp+=${diff-grammar}" "$@"\n' "$nvim_bin" > "$tmpdir/nvim"
+            chmod +x "$tmpdir/nvim"
+            PATH="$tmpdir:$PATH" exec ${luaEnv}/bin/busted "$@"
+          '';
+          commonPackages = [
+            busted-with-grammar
+            pkgs.prettier
+            pkgs.stylua
+            pkgs.selene
+            pkgs.lua-language-server
+            vimdoc-language-server.packages.${pkgs.system}.default
+          ];
+        in
+        {
+          default = pkgs.mkShell {
+            packages = commonPackages;
           };
-      });
+          ci = pkgs.mkShell {
+            packages = commonPackages ++ [ pkgs.neovim ];
+          };
+        }
+      );
     };
 }
