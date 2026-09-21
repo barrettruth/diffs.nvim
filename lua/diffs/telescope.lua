f85d1abc30d78566987751b5093329b52c299bff
Barrett Ruth <62671086+barrettruth@users.noreply.github.com>
2026-09-21 10:58:26 -0500

    perf(startup): defer implementation modules (#435)

--- lua/diffs/telescope.lua
+++ lua/diffs/telescope.lua
@@ -1,13 +1,13 @@
 local M = {}
 
-function M.setup()
-  local runtime = require('diffs.runtime')
+---@param attach fun(bufnr: integer)
+function M.setup(attach)
   local group = vim.api.nvim_create_augroup('diffs_telescope', { clear = true })
   return vim.api.nvim_create_autocmd('User', {
     group = group,
     pattern = 'TelescopePreviewerLoaded',
     callback = function()
-      runtime.attach(vim.api.nvim_get_current_buf())
+      attach(vim.api.nvim_get_current_buf())
     end,
   })
 end
